43d7ed5ebe9e2d5664af90cbb5c901a06d7427ba
Sachin Verma <sachin.verma@rxlogix.com>
2023-01-20 14:36:39 +0530

    PVR-33676 Added fix for attachments data encode issue.

--- src/main/groovy/grails/plugins/mail/graph/sender/GraphMailMessageBuilderFactory.groovy
+++ src/main/groovy/grails/plugins/mail/graph/sender/GraphMailMessageBuilderFactory.groovy
@@ -5,13 +5,16 @@ import grails.plugins.mail.MailMessageBuilder
 import grails.plugins.mail.MailMessageBuilderFactory
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
+import org.grails.web.mime.DefaultMimeUtility
 
 @Slf4j
 @CompileStatic
 class GraphMailMessageBuilderFactory extends MailMessageBuilderFactory {
 
+    DefaultMimeUtility grailsMimeUtility
+
     MailMessageBuilder createBuilder(Config config) {
-        new GraphMailMessageBuilder(mailSender, config, mailMessageContentRenderer)
+        new GraphMailMessageBuilder(mailSender, config, mailMessageContentRenderer, grailsMimeUtility)
     }
 
 }
